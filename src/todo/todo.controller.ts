ad4c2a769df97128aa6dec4142b49c0fa1f51223
selimsoussi1 <selim.soussi@insat.ucar.tn>
2025-10-10 02:23:47 +0100

    create addTodo pour ajouter dans la base de données

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,9 +1,11 @@
 import { Body, Controller, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { TodoService } from './todo.service';
 
 @Controller('todo')
 export class TodoController {
+    constructor(private readonly todoService: TodoService) {}
   @Post()
   @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
   create(@Body() createTodoDto: CreateTodoDto) {
@@ -15,4 +17,5 @@ export class TodoController {
   update(@Body() updateTodoDto: UpdateTodoDto) {
     return updateTodoDto;
   }
+  
 }
---
+++ src/todo/todo.service.ts
@@ -0,0 +1,29 @@
+// src/todo/todo.service.ts
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TodoEntity } from './todo.entity';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { StatusEnum } from './status.enum';
+
+@Injectable()
+export class TodoService {
+  constructor(
+    @InjectRepository(TodoEntity)
+    private readonly todoRepository: Repository<TodoEntity>,
+  ) {}
+
+ // Ajouter un Todo dans la base
+  async addTodo(createTodoDto: CreateTodoDto): Promise<TodoEntity> {
+    // Création de l'entité Todo
+    const todo = this.todoRepository.create({
+      name: createTodoDto.name,
+      description: createTodoDto.description,
+      status: createTodoDto.status ? StatusEnum[createTodoDto.status.toUpperCase()] : undefined, // utilise l'enum StatusEnum
+    });
+
+    // Sauvegarde dans la base et retour de l'objet complet
+     return this.todoRepository.save(todo);
+    
+  }
+}
