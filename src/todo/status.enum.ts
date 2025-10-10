1345a5598a6b8cc152788afb9705827d87960258
selimsoussi1 <selim.soussi@insat.ucar.tn>
2025-10-10 01:09:23 +0100

    Create TodoEntity

---
+++ src/todo/status.enum.ts
@@ -0,0 +1,5 @@
+export enum StatusEnum {
+  PENDING = 'PENDING',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE',
+}
---
+++ src/todo/todo.entity.ts
@@ -0,0 +1,27 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { StatusEnum } from './status.enum';
+
+
+@Entity('todo')
+
+export class TodoEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+
+    @Column()
+    name: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    createdAt: Date;
+
+    @Column({
+    type: 'enum',
+    enum: StatusEnum,
+    default: StatusEnum.PENDING
+    })
+status: StatusEnum;
+}
